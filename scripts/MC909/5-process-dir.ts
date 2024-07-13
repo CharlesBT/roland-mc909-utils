9a4fa66c0492d56f4dac696a99c546715405f429
CharlesBT <charles.brebant@gmail.com>
2024-07-13 11:11:39 +0200

    -

--- scripts/MC909/5-process-dir.ts
+++ scripts/MC909/5-process-dir.ts
@@ -5,6 +5,13 @@ const SAMPLE_ROOT_DIR = 'H:/_MC909'
 
 const MC909Samples = useMC909Samples()
 MC909Samples.updateDirWithRolandChunk(SAMPLE_ROOT_DIR)
-MC909Samples.renameFiles(SAMPLE_ROOT_DIR, 3) // slot starting to 3 to keep an empty slot for stereo sampling
+
+/*
+WARNING if there are empty slot between samples, the loading is longer
+TONE_PATCHES slots : 3-2000, slot starting to 3 to keep an empty slot for stereo sampling
+RHYTHM_PATCHES slots : 2000-7000 for loops, drums, hits, vocals, fx ...
+*/
+
+MC909Samples.renameFiles(SAMPLE_ROOT_DIR, 3)
 
 writeReport()
